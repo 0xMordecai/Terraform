4bfa0c37a8df840e078195f04903f763546c74e7
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-08-01 22:46:52 +0100

    Use variables in code

--- main/Rg.tf
+++ main/Rg.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg"{
-	name = "bookRg"
-	location = "West Europe"
+	name = var.resource_group_name
+	location = var.location
 	tags {
 		environment = "Terraform Azure"
 	}
